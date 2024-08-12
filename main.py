14f5c8d6b5e24b10b6a19dc1d3abf5fe75b5c347
domingasp <d.pleck@outlook.com>
2024-08-12 19:26:46 +0100

    refactor: create PySide6 project through Qt Creator
    Moving from VS Code to Qt Creator to improve developer experience through
    QML auto-complete functionality.

--- main.py
+++ main.py
@@ -1,35 +1,9 @@
-import logging
+# This Python file uses the following encoding: utf-8
 import sys
 from PySide6.QtWidgets import QApplication
 
-from logging_config import setup_logging
-from ui.OverlayApp import OverlayApp
-
-setup_logging()
-
-logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    overlay_app = OverlayApp(app=app)
+    # ...
     sys.exit(app.exec())
-
-    # if not engine.rootObjects():
-    #     sys.exit(-1)
-
-    # app = QApplication(sys.argv)
-    # app.setQuitOnLastWindowClosed(False)
-
-    # with open("ui/styles/styles.qss", "r") as file:
-    #     stylesheet = file.read()
-    # app.setStyleSheet(stylesheet)
-
-    # overlay = Overlay()
-    # overlay.showFullScreen()
-
-    # ws_client = WebSocketClient(overlay)
-    # threading.Thread(target=ws_client.run, daemon=True).start()
-
-    # overlay.set_websocket_client(websocket_client=ws_client)
-
-    # sys.exit(app.exec())
